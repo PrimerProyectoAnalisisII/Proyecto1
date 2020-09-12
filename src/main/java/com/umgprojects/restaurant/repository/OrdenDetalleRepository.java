ebec2fe4ea42e3907435b4a47a74f3a5e6f0b624
Jessica Calderon <jcalderonh@miumg.edu.gt>
2020-09-12 13:18:03 -0600

    Se agregan los repository del servicio de Orden

---
+++ src/main/java/com/umgprojects/restaurant/repository/OrdenDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.umgprojects.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.umgprojects.restaurant.entity.OrdenDetalle;
+
+public interface OrdenDetalleRepository extends JpaRepository<OrdenDetalle, Integer> {
+
+}
