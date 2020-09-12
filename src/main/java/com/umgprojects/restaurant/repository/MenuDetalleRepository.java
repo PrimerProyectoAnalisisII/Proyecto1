863860813557085a32bee7ac3cc9e0edabde5cda
Alejandro Montalvo <cmontalvom@miumg.edu.gt>
2020-09-12 13:34:51 -0600

    Se agregan las interfaces para los metodos CRUD del servicio Menu

---
+++ src/main/java/com/umgprojects/restaurant/repository/MenuDetalleRepository.java
@@ -0,0 +1,9 @@
+package com.umgprojects.restaurant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.umgprojects.restaurant.entity.MenuDetalle;
+
+public interface MenuDetalleRepository extends JpaRepository<MenuDetalle, Integer> {
+
+}
